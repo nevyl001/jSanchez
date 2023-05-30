262b96996a64fdccf3977f778c465ae8e8640bc2
NevylDeveloper <95713035+nevyl001@users.noreply.github.com>
2023-05-30 15:20:38 -0600

    Revert "changestoday"

--- app/layout.js
+++ app/layout.js
@@ -7,7 +7,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="ES">
+    <html lang="en">
       <body>{children}</body>
     </html>
   );
